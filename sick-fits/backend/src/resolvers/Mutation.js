db336c192ee0cdaf7bbdf1d990a47361a8178f63
eddie chung <eddie.chung@moonfare.com>
2019-09-19 17:41:28 +0200

    setup schema

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctz, info) {
+    //TODO: check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
