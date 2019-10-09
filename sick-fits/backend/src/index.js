c40e1b2e672f8d12ceb23c4085b3beb69f1ed2d9
Eddie Chung <designered@gmail.com>
2019-10-09 23:17:37 +0200

    setup user, cookies and reder props component

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -7,6 +8,18 @@ const server = createServer();
 
 // TODO Use express middlware to handle cookies (JWT)
 server.express.use(cookieParser());
+
+//decode the JWT so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
+
 // TODO Use express middlware to populate current user
 
 server.start(
